7f8442ed059951dd00e90d235a02bd7ca174a3a9
abhaysinghchauhan009 <chauhanabhay781@gmail.com>
2025-03-28 22:42:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002046472370')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/NOBITA_MOVIES_SEARCH') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/+Ds37-TCBfGBhYjk1")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/+Ds37-TCBfGBhYjk1")
