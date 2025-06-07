22a863743628a1ca5da1a8bf1d6eb276c3415250
abhaysinghchauhan009 <chauhanabhay781@gmail.com>
2025-06-07 18:12:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5536473064 7763229951').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/ll_YOURR_NOBITA_ll") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/ll_NOBITA_DEFAULTERS_ll") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002567576862'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/NOBITA_MOVIES_SEARCH')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002367446556').split()]
